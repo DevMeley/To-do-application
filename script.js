95b902541aced293b00969a651d9ea1455617afe
DevMeley <ayantolaAbisola@outlook.com>
2024-09-25 08:12:10 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const myInput = document.getElementById("myInput")
+const myCancelbtn = document.getElementById("myCancelbtn")
+const myAddButton = document.getElementsByClassName("myAddButton")[0]
+const myList = document.getElementById("myList")
+
+
+myAddButton.addEventListener('click' , event => {
+    if (myInput.value === '') {
+        alert("You have to make a list")
+    }
+    else{
+        const li = document.createElement("li")
+        li.innerHTML = myInput.value
+        myList.append(li)
+    }
+
+})
+
+myInput.value === ''
